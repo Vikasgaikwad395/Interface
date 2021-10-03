5902c6da65624455b4f020e286504ea5fae1a4f6
Vikasgaikwad395 <60402722+Vikasgaikwad395@users.noreply.github.com>
2021-10-04 01:36:54 +0530

    Update MyCalculator.java

--- MyCalculator.java
+++ MyCalculator.java
@@ -1,4 +1,33 @@
-
-public class MyCalculator {
-
+interface Calculator{
+	int x = 20;
+	int y = 30;
+	void add();
+	void sub();
+	void mul();
+	void div();
+	
+	
 }
+public class MyCalculator implements Calculator{
+ 
+	public void add() {
+		System.out.println("Addition is = "+(x+y));
+	}
+	public void sub() {
+		System.out.println("Substraction  is = "+(x-y));
+	}
+	public void mul() {
+		System.out.println("Mutliplication is = "+(x*y));
+	}
+	public void div() {
+		System.out.println("Division is = "+(x/y));
+	}
+	public static void main(String[] args) {
+		MyCalculator m = new MyCalculator();
+		m.add();
+		m.sub();
+		m.mul();
+		m.div();
+	}
+}
+
